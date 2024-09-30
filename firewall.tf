874da3dfea94030083a4365431163a1d0ec232b1
Girish Sivakumar <girish_intrests@yahoo.com>
2024-09-30 01:12:32 +0000

    first commit of my first terraform project where I created some terraform code as part of Module4 Lab5 of GCP training

---
+++ firewall.tf
@@ -0,0 +1,14 @@
+# Add a firewall rule to allow HTTP, SSH, RDP, and ICMP traffic on mynetwork
+resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
+  name    = "mynetwork-allow-http-ssh-rdp-icmp"
+  network = google_compute_network.mynetwork.self_link
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3389"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+
+  source_tags = ["web"]
+}
---
+++ vpc.tf
@@ -0,0 +1,19 @@
+# Create the mynetwork network
+resource "google_compute_network" "mynetwork" {
+  name                    = "mynetwork"
+  auto_create_subnetworks = true
+}
+# Create the mynet-us-vm instance
+module "mynet-us-vm" {
+  source           = "./instance"
+  instance_name    = "mynet-us-vm"
+  instance_zone    = "us-central1-c"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+# Create the mynet-eu-vm" instance
+module "mynet-eu-vm" {
+  source           = "./instance"
+  instance_name    = "mynet-eu-vm"
+  instance_zone    = "europe-west1-d"
+  instance_network = google_compute_network.mynetwork.self_link
+}
